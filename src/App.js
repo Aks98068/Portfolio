23182f6735b2b84d49e4183b8de336f5cc5927b7
Gyanendra Sah <gyanee750@gmail.com>
2025-04-27 20:32:52 +0545

    deploy ko lagi:

--- src/App.js
+++ src/App.js
@@ -4,20 +4,17 @@ import Main from "./containers/Main";
 import { ThemeProvider } from "styled-components";
 import { chosenTheme } from "./theme";
 import { GlobalStyles } from "./global";
-import { BrowserRouter } from "react-router-dom";
 
 function App() {
   return (
-    <BrowserRouter basename="/Portfolio">
-      <ThemeProvider theme={chosenTheme}>
-        <>
-          <GlobalStyles />
-          <div>
-            <Main theme={chosenTheme} />
-          </div>
-        </>
-      </ThemeProvider>
-    </BrowserRouter>
+    <ThemeProvider theme={chosenTheme}>
+      <>
+        <GlobalStyles />
+        <div>
+          <Main theme={chosenTheme} />
+        </div>
+      </>
+    </ThemeProvider>
   );
 }
 
